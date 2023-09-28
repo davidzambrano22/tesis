05b19c6087aa11481c351c5285710a13ea59f0e0
davidzambrano22 <adariasz@unal.edu.co>
2023-09-28 18:36:08 -0500

    big network modified

--- analisis_ortogrupos/code/networks/functions.R
+++ analisis_ortogrupos/code/networks/functions.R
@@ -31,16 +31,31 @@ get.highCorr.genes <- function(cor.matrix, threshold){
   pos <- 1
   for (i in rownames(cor.matrix)){
     print(pos)
-    print(i)
     corr.names <- rownames(cor.matrix[rownames(cor.matrix) != i,])
     correlated.hogs <- colnames(cor.matrix[,cor.matrix[i, corr.names] >= threshold])
     if (length(correlated.hogs) != 0){
-      print(correlated.hogs)
       high.corr.genes <- c(high.corr.genes, c(i, correlated.hogs))
     }
-    pos <- pos +1 
+    pos <- pos +1
   }
   return(names(table(high.corr.genes)))
 }
 
+
+getHighcorGenes <- function(expression.matrix, threshold){
+  high.corr.genes <- c()
+  
+  nrows <- 1:dim(expression.matrix)[1]
+  for (i in nrows){
+    for (j in nrows){
+      print(cat(i, j))
+      if (i == j) next
+      corr <- abs(cor(unlist(expression.matrix[i,]), unlist(expression.matrix[j,])))
+      print(cat("Corr is = ", corr))
+      if (is.na(corr)) next
+      else if (corr >= threshold) {high.corr.genes <- c(high.corr.genes, rownames(expression.matrix[i,]), rownames(expression.matrix[i+1,]))} 
+    }
+  }
+  return(high.corr.genes)
+}
 #==================================================================
