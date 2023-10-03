3707e1b01ed304f7eaec86065b7f72d4feb46043
davidzambrano22 <adariasz@unal.edu.co>
2023-10-03 12:21:12 -0500

    enhanced network scripts

--- analisis_ortogrupos/code/networks/functions.R
+++ analisis_ortogrupos/code/networks/functions.R
@@ -70,34 +70,62 @@ view.hogs.degree <- function(degree.frame, n = F){
   # (This function return a list with info about nodes degree) #
   if (is.numeric(n)){
     highest.degrees <- degree.frame %>% arrange(desc(Degree))
-    #View(highest.degrees[1:n,])
+    View(highest.degrees[1:n,])
     return(list(Names = unlist(highest.degrees[1:n,]$`X`), Degrees = unlist(highest.degrees[1:n,]$`Degree`)))
   } else {
     highest.degrees <- degree.frame %>% arrange(desc(Degree))
-    #View(highest.degrees)
+    View(highest.degrees)
   }
 }
 
-get.central.HOGS.forNode <- function(nodes.file, degree.list){
-  node.frame <<- read.csv(nodes.file)
+get.central.HOGS.forNode <- function(modules.file, degree.list){
+  modules.frame <<- read.csv(modules.file)
   message("Modules...")
-  modules <- table(node.frame[,2])
+  modules <<- table(modules.frame[,2])
   print(modules)
   n.modules <- length(modules)
   message(sprintf("Network has %s modules; most connected HOGS are in modules: ", n.modules))
   
   for (h in degree.list$Names){
-    print(cat(h, "= Node ", node.frame[node.frame[,1] == h, 2]))
+    print(cat(h, "= Node ", modules.frame[modules.frame[,1] == h, 2]))
   }
 }
 
 connected.Hogs.forNode <- function(degree.frame, module, n){
-  hogs <- node.frame[node.frame[,2] == module, 1]
+  hogs <- modules.frame[modules.frame[,2] == module, 1]
   degree.frame.filtered <- degree.frame[degree.frame$X %in% hogs,] %>% arrange(desc(Degree))
   degree.frame.filtered <-  degree.frame.filtered[1:n,]
-  print(" HOG   DEGREE")
+  print(" HOG       DEGREE")
   for (x in unlist(degree.frame.filtered$`X`)){
     print(cat(x, degree.frame.filtered[degree.frame.filtered$`X` == x, "Degree"]))
   }
+  return(degree.frame.filtered)
 }
 
+
+# ====================== MODULES ANALYSIS ===========
+draw.flavonoidInModules <- function(){
+  modules.vector <- c()
+  modules.HOGs.vector <- c()
+  flav.HOGS.in.vector <- c()
+  
+  for (m in as.numeric(names(modules))){
+    modules.vector <- c(modules.vector, m)
+    
+    module.HOGs <- modules.frame[modules.frame[,2] == m, 1]
+    n.module.HOGs <- length(module.HOGs)
+    modules.HOGs.vector <- c(modules.HOGs.vector, n.module.HOGs)
+    
+    flav.HOGS.in <- module.HOGs[module.HOGs %in% floavonoid.path.HOGS]
+    n.flav.HOGS.in <- length(flav.HOGS.in)
+    flav.HOGS.in.vector <- c(flav.HOGS.in.vector, n.flav.HOGS.in)
+  }
+  print(length(modules.vector))
+  print(length(modules.HOGs.vector))
+  print(length(flav.HOGS.in.vector))
+  
+  bar.data <- matrix(c(modules.HOGs.vector, flav.HOGS.in.vector), nrow = 2, byrow = TRUE)
+  barplot(bar.data, beside = TRUE, col = c("blue", "red"), names.arg = modules.vector,
+          legend.text = c("Module", "Flavonoid Hogs"), 
+          main = "Grouped Barplot of Modules and Flavonoid HOGs")
+}
